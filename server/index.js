826b99fd258ea8aa6efd4bf70e64129f5dd62ab0
afit08 <afitrabbani@gmail.com>
2023-12-27 10:23:30 +0700

    update Galleries and clustering

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-// 1. pastikan selalu import dotenv di line pertama
-//import "dotenv/config";
+// import "dotenv/config";
 import config from './config/config';
 import express from 'express';
 import cors from 'cors';
@@ -9,68 +8,94 @@ import helmet from 'helmet';
 import middleware from './helpers/middleware';
 import morgan from 'morgan';
 
-//for access models to db
+// for access models to db
 import models, { sequelize } from './models/init-models';
 import routes from './routes/IndexRoute';
+import os from 'os';
+import cluster from 'cluster';
 
 // declare port
 const port = process.env.PORT || 3000;
 
-const app = express();
-// parse body params and attache them to req.body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-// use helmet spy bisa dikenali SEO
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-    },
-  }),
-);
-app.use(helmet.hsts({ maxAge: 2147483648 }));
-// secure apps by setting various HTTP headers
-app.use(compress());
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
+const clusterWorkerSize = os.cpus().length;
 
-// load models dan simpan di req.context
-app.use(async (req, res, next) => {
-  req.context = { models };
-  next();
-});
+if (clusterWorkerSize > 1) {
+  if (cluster.isMaster) {
+    for (let i = 0; i < clusterWorkerSize; i++) {
+      cluster.fork();
+    }
 
-app.use(morgan('dev'));
-
-// call routes
-app.use(config.URL_API + '/auth', routes.UserRoute);
-app.use(config.URL_API + '/roles', routes.RolesRoute);
-app.use(config.URL_API + '/categories', routes.CategoriesRoute);
-app.use(config.URL_API + '/products', routes.ProductsRoute);
-app.use(config.URL_API + '/carts', routes.CartsRoute);
-app.use(config.URL_API + '/paymentMethod', routes.PaymentRoute);
-app.use(config.URL_API + '/ongkir', routes.OngkirRoute);
-app.use(config.URL_API + '/galleries', routes.GalleriesRoute);
-app.use(config.URL_API + '/address', routes.AddressRoute);
-app.use(config.URL_API + '/orders', routes.OrderRoute);
-
-//use middleware to handle error from others modules
-app.use(middleware.handleError);
-app.use(middleware.notFound);
-
-// set to false agar tidak di drop tables yang ada didatabase
-const dropDatabaseSync = false;
-
-sequelize.sync({ force: dropDatabaseSync }).then(async () => {
-  if (dropDatabaseSync) {
-    console.log('Database do not drop');
+    cluster.on('exit', function (worker) {
+      console.log('Worker', worker.id, ' has exitted.');
+    });
+  } else {
+    createServer();
   }
+} else {
+  createServer();
+}
 
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+function createServer() {
+  const app = express();
+  // parse body params and attach them to req.body
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  // use helmet spy bisa dikenali SEO
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+      },
+    }),
+  );
+  app.use(helmet.hsts({ maxAge: 2147483648 }));
+  // secure apps by setting various HTTP headers
+  app.use(compress());
+  // enable CORS - Cross Origin Resource Sharing
+  app.use(cors());
+
+  // load models dan simpan di req.context
+  app.use(async (req, res, next) => {
+    req.context = { models };
+    next();
   });
-});
 
-export default app;
+  app.use(morgan('dev'));
+
+  // call routes
+  app.use(config.URL_API + '/auth', routes.UserRoute);
+  app.use(config.URL_API + '/roles', routes.RolesRoute);
+  app.use(config.URL_API + '/categories', routes.CategoriesRoute);
+  app.use(config.URL_API + '/products', routes.ProductsRoute);
+  app.use(config.URL_API + '/carts', routes.CartsRoute);
+  app.use(config.URL_API + '/paymentMethod', routes.PaymentRoute);
+  app.use(config.URL_API + '/ongkir', routes.OngkirRoute);
+  app.use(config.URL_API + '/galleries', routes.GalleriesRoute);
+  app.use(config.URL_API + '/address', routes.AddressRoute);
+  app.use(config.URL_API + '/orders', routes.OrderRoute);
+
+  // use middleware to handle errors from other modules
+  app.use(middleware.handleError);
+  app.use(middleware.notFound);
+
+  // set to false agar tidak di drop tables yang ada didatabase
+  const dropDatabaseSync = false;
+
+  sequelize.sync({ force: dropDatabaseSync }).then(async () => {
+    if (dropDatabaseSync) {
+      console.log('Database do not drop');
+    }
+
+    app.listen(port, () => {
+      console.log(
+        `Server is listening on port ${port} ${'with multiple workers'} ${
+          process.pid
+        }`,
+      );
+    });
+  });
+}
+
+export default createServer;
--- server/routes/GalleriesRoute.js
+++ server/routes/GalleriesRoute.js
@@ -1,32 +1,56 @@
 import { Router } from 'express';
 import IndexController from '../controller/IndexController';
 import UploadDownloadHelper from '../helpers/UploadDownloadHelper';
+import authJWT from '../helpers/authJWT';
 
 const router = Router();
-
-router.get('/', IndexController.GalleriesController.allGalleries);
+// CUSTOMER
+router.get(
+  '/customer/all',
+  authJWT.ensureCustomer,
+  IndexController.GalleriesController.allGalleries,
+);
+// ADMIN
+router.get(
+  '/admin/all',
+  authJWT.ensureAdmin,
+  IndexController.GalleriesController.allGalleries,
+);
 router.post(
   '/store',
+  authJWT.ensureAdmin,
   UploadDownloadHelper.uploadSingleFiles,
   IndexController.GalleriesController.createGalleries,
 );
-router.get('/image/:filename', UploadDownloadHelper.showProductImage);
+router.get(
+  '/image/:filename',
+  authJWT.ensureAdmin,
+  UploadDownloadHelper.showProductImage,
+);
 router.post(
   '/edit/:id',
+  authJWT.ensureAdmin,
   UploadDownloadHelper.uploadSingleFiles,
   IndexController.GalleriesController.updateGalleries,
 );
 router.post(
   '/editNoImage/:id',
+  authJWT.ensureAdmin,
   IndexController.GalleriesController.updateGalleriesNoImage,
 );
 router.delete(
   '/delete/:id',
+  authJWT.ensureAdmin,
   IndexController.GalleriesController.deleteGalleries,
 );
-router.get('/:id', IndexController.GalleriesController.detailGalleries);
+router.get(
+  '/:id',
+  authJWT.ensureAdmin,
+  IndexController.GalleriesController.detailGalleries,
+);
 router.post(
   '/searchCategories',
+  authJWT.ensureAdmin,
   IndexController.GalleriesController.allGalleriesSearch,
 );
 
