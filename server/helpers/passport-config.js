bfc8e87bdbbbe1d1893de127eed1fffa7eced931
afit08 <afitrabbani@gmail.com>
2024-04-03 08:39:54 +0700

    update

--- server/helpers/passport-config.js
+++ server/helpers/passport-config.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+require('dotenv').config();
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        '199944315367-u54rliij8rgijgfip7nanaqq3fi9qg5e.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_URL,
     },
     (accessToken, refreshToken, profile, done) => {
       // You can perform database operations here, e.g., find or create user
